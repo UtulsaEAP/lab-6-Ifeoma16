1b19d36a837b4b037642f08193f4a2ddf05dbb24
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-29 16:05:15 -0600

    problem 6

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -1,14 +1,24 @@
-def process_user_contacts(user_input):
-    user_contacts = 
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
 
- 
-    user_input = 
-    tokens = 
+def process_user_contacts(user_input):
+    user_contacts = {}
+
+    tokens = user_input.split()
 
     # Put word pairs into a dictionary
+    for i in range(0, len(tokens), 2):
+        name = tokens[i]
+        phone_num = tokens[i + 1]
+        user_contacts[name] = phone_num
     
     # Get contact name from input, output contact's phone number
     contact_name = input("Enter the contact name: ")
+
+    if contact_name in user_contacts:
+        print(user_contacts[contact_name])
     
    
 if __name__ == '__main__':
