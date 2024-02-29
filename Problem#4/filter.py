34876a2f26c24d9e7f69a14d6513e7963f4f0d7f
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-29 15:29:36 -0600

    problem 4

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -1,12 +1,21 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
+
 def process_and_print(input_string):
-      # Split into separate strings
+    # Split into separate strings
+    numbers = input_string.split()
 
     # Convert strings to integers and filter out negative values
-    input_data = 
+    input_data = [int(num) for num in numbers if int(num) < 0]
 
     # Sort integers in reverse order
+    input_data.sort(reverse = True)
   
     # Print sorted integers
+    for num in input_data:
+        print(num, end = ' ')
     
 
 if __name__ == "__main__":
