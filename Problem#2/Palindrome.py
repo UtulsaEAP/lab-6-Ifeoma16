40157d4d14daf889c750f7178f595ec92ef91bd5
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-29 15:03:36 -0600

    problem 2

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,5 +1,15 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
+
 def check_palindrome(user_input):
  #type your code here
+    if user_input[::-1] == user_input:
+        print("palindrome:", user_input)
+    else:
+        print("not a palindrome:", user_input)
+
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
