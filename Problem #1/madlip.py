b79b32fbe5ff6723bcf12e3dd30bfe74bab154ed
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-29 14:56:56 -0600

    problem 1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,7 +1,22 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
+
 def food_input():
-    user_input = input()
-    tokens = user_input.split()
     #type you while  loop here
+    while True:
+        user_input = input()
+        tokens = user_input.split()
+
+        if len(tokens) == 2 and tokens[0].lower() == 'quit' and int(tokens[1]) == 0:
+            break
+        if len(tokens) == 2:
+            item = tokens[0]
+            quantity = int(tokens[1])
+
+            print("Eating", quantity, item, "a day keeps you happy and healthy.")
+    
 
     
 
