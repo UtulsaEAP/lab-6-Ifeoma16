e157e302071011ee4e58c218d7aa6d33ed610197
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-29 16:22:13 -0600

    problem 7

--- Problem#7/carwash.py
+++ Problem#7/carwash.py
@@ -1,9 +1,29 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
+
 def calculate_car_wash_price(service_choice1, service_choice2):
     services = {'Air freshener': 1, 'Rain repellent': 2, 'Tire shine': 2, 'Wax': 3, 'Vacuum': 5}
     base_wash = 10
-    total = 0
+    total = base_wash
    
-   #type your code here 
+   #type your code here
+    print("ZyCar Wash")
+    print(f"Base car wash - ${base_wash}")
+
+    if service_choice1 != '-':
+        cost1 = services.get(service_choice1, 0)
+        total += cost1
+        print(f"{service_choice1} - ${cost1}")
+    
+    if service_choice2 != '-':
+        cost2 = services.get(service_choice2, 0)
+        total += cost2
+        print(f"{service_choice2} - ${cost2}")
+
+    print(f"Total price: ${total}")
+
 
     
 if __name__ == '__main__':
