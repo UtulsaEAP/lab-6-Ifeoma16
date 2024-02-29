00b5ecfc3d89c4884ca8086cf7e88b92e64d420a
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-29 15:18:04 -0600

    problem 3

--- Problem#3/varied.py
+++ Problem#3/varied.py
@@ -1,13 +1,23 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
+
+from statistics import mean
+
 def process_input(input_string):
-      # Split into separate strings
+    # Split into separate strings
+    numbers = input_string.split()
 
     # Convert strings to floats
-    
+    floatnum = [float(num) for num in numbers]
 
-    # Get max and average
-    max_value = 
-    average_value = 
-    return max_value, average_value
+    if all(float_num > 0 for float_num in floatnum):
+      # Get max and average
+      max_value = max(floatnum)
+      average_value = mean(floatnum)
+      return max_value, average_value
+    
 
 if __name__ == "__main__":
     # User inputs string w/ numbers
